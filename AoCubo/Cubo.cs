22620ccf027160dec0e791df05a78466a907c36c
Everton carvalho <everton.de.carvalho@hotmail.com.br>
2022-06-06 16:13:32 -0300

    Adicionar arquivos de projeto.

---
+++ AoCubo/Cubo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCubo
+{
+    public class Cubo
+    {
+        public double Aocubo(ref int num)
+        {
+            return num = num * num * num;
+        }
+               
+    }
+
+    
+}
---
+++ AoCubo/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AoCubo;
+
+
+
+
+Cubo teste = new Cubo();
+
+string entrada = Console.ReadLine();
+int numero = int.Parse(entrada);
+
+teste.Aocubo(ref numero);
+
+Console.WriteLine($" O valor do cubo é {numero}.");
+
+
+
+
+
+
+
+
